8803694a083ff31cc86ff7ad16ad6a852ddfc4b0
Thomas Kelly <tmkelly28@gmail.com>
2017-03-08 12:29:35 -0500

    saving

--- client/components/ChannelList.js
+++ client/components/ChannelList.js
@@ -1,48 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 import store from '../store';
 
-// hardcoded...for now!
-const RANDOM_CHANNEL = '/channels/1';
-const GENERAL_CHANNEL = '/channels/2';
+function ChannelList (props) {
 
-export default class ChannelList extends Component {
+  const { messages, channels } = props;
 
-  constructor () {
-    super();
-    this.state = store.getState();
-  }
-
-  componentDidMount () {
-    this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
-  }
-
-  componentWillUnmount () {
-    this.unsubscribe();
-  }
-
-  render () {
-
-    const { messages } = this.state;
-
-    return (
-      <ul>
-        <li>
-          <Link to={RANDOM_CHANNEL}>
-            <span># really random</span>
-            <span className="badge">{ messages.filter(message => message.channelId === 1).length }</span>
-          </Link>
-        </li>
-        <li>
-          <Link to={GENERAL_CHANNEL}>
-            <span># generally speaking</span>
-            <span className="badge">{ messages.filter(message => message.channelId === 2).length }</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/new-channel">Create a channel...</Link>
-        </li>
-      </ul>
-    );
-  }
+  return (
+    <ul>
+      {
+        channels.map(channel => {
+          return (
+            <li key={channel.id}>
+              <Link to={`/channels/${channel.id}`}>
+                <span># {channel.name}</span>
+                <span className="badge">{ messages.filter(message => message.channelId === channel.id).length }</span>
+              </Link>
+            </li>
+          )
+        })
+      }
+      <li>
+        <Link to="/new-channel">Create a channel...</Link>
+      </li>
+    </ul>
+  );
 }
+
+const mapStateToProps = function (state) {
+  return {
+      messages: state.messages,
+      channels: state.channels
+  };
+};
+
+export default connect(mapStateToProps)(ChannelList);
